01a42d1da90239ceda0785652acf2b1f9a2161b8
Caesar-12 <idowuenoch65@gmail.com>
2022-11-29 16:06:24 -0800

    Reverse alphabets in alternating cases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+start = 122
+for i in range(0, 26):
+    print("{:s}".format(chr(start)), end="")
+    if (i % 2) == 0:
+        start -= 32
+    else:
+        start += 32
+    start -= 1
