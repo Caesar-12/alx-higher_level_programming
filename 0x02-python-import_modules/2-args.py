df56e9ea1aaa3bcf9d687171c890ac8db6962f7c
Caesar-12 <idowuenoch65@gmail.com>
2022-12-01 07:52:04 -0800

    strings error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
 
-    arg_c = len(sys.argv)
+    arg_c = len(sys.argv) - 1
     if arg_c > 1:
-        print("{:d} arguments:".format(arg_c - 1))
+        print("{:d} arguments:".format(arg_c))
     else:
-        print("{:d} argument:".format(arg_c - 1))
+        print("{:d} argument.".format(arg_c))
 
-    for i in range(1, arg_c):
+    for i in range(1, arg_c + 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
