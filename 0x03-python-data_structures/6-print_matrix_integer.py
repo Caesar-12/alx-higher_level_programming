011c2b1247b2865ec8df3eca29b5706d46fbabd2
Caesar-12 <idowuenoch65@gmail.com>
2022-12-03 04:19:14 -0800

    Lists and tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
+        size = 1
         for num in lists:
-            print("{:d} ".format(num), end="")
+            if size < len(lists):
+                print("{:d} ".format(num), end="")
+            elif size == len(lists):
+                print("{:d}".format(num), end="")
+            size += 1
         print()
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lfl = len(tuple_a)
+    lsl = len(tuple_b)
+    if tuple_a or tuple_b:
+        if lfl == 2 and lsl == 2:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        elif lfl == 2 and lsl == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[0])
+        elif lfl == 2 and lsl == 0:
+            new_tuple = (tuple_a[0], tuple_a[1])
+        elif lfl == 0 and lsl == 2:
+            new_tuple = (tuple_b[0], tuple_b[1])
+        elif lfl == 1 and lsl == 2:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
+        elif lfl == 0 and lsl == 0:
+            new_tuple = ()
+        elif lfl == 1 and lsl == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0])
+        return new_tuple
