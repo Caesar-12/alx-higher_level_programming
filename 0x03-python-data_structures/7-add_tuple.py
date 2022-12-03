318e2f34af73b83d90f4144a7d7a2b6c2c714c3b
Caesar-12 <idowuenoch65@gmail.com>
2022-12-03 04:43:11 -0800

    updated conditions

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,19 +2,18 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     lfl = len(tuple_a)
     lsl = len(tuple_b)
-    if tuple_a or tuple_b:
-        if lfl >= 2 and lsl >= 2:
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        elif lfl >= 2 and lsl == 1:
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[0] + 0)
-        elif lfl >= 2 and lsl == 0:
-            new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
-        elif lfl == 0 and lsl >= 2:
-            new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
-        elif lfl == 1 and lsl >= 2:
-            new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-        elif lfl == 0 and lsl == 0:
-            new_tuple = (0, 0)
-        elif lfl == 1 and lsl == 1:
-            new_tuple = (tuple_a[0] + 0, 0 + tuple_b[0])
-        return new_tuple
+    if lfl >= 2 and lsl >= 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif lfl >= 2 and lsl == 1:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[0] + 0)
+    elif lfl >= 2 and lsl == 0:
+        new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+    elif lfl == 0 and lsl >= 2:
+        new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
+    elif lfl == 1 and lsl >= 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    elif lfl == 0 and lsl == 0:
+        new_tuple = (0, 0)
+    elif lfl == 1 and lsl == 1:
+        new_tuple = (tuple_a[0] + 0, 0 + tuple_b[0])
+    return new_tuple
