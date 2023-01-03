138bcb18341ad71664ad2ca3adea6aa3f8e3acf7
Caesar-12 <idowuenoch65@gmail.com>
2023-01-02 19:15:08 -0800

    Static and class methods

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+"""
+Module: 0-rectangle.py
+Defines a class -> Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Class: Rectangle
+    Declares 2 private class attributes, 2 public class and 9 methods
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        shapeList = []
+
+        for i in range(self.__height):
+            shapeList.append(str(self.print_symbol) * self.__width)
+        rec = "\n".join(shapeList)
+        return rec
+
+    def __repr__(self):
+        return "Rectangle("+str(self.__width)+", "+str(self.__height)+")"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        elif rect_2.area() > rect_1.area():
+            return rect_2
+        else:
+            return rect_1
