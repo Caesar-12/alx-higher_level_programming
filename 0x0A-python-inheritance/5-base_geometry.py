99ac3ab5706361f604900d885cd173e28e118879
Caesar-12 <idowuenoch65@gmail.com>
2023-01-09 16:04:28 -0800

    inhwritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module: 5-base_geometry
+empty class
+"""
+
+
+class BaseGeometry():
+    """empty class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module: B-base_geometry
+checks area"
+"""
+
+
+class BaseGeometry():
+    """contains unimplemented method"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
