c807bcd0b35f8e3692214cada2979e0e8f2c0ba9
Caesar-12 <idowuenoch65@gmail.com>
2023-03-13 15:13:06 -0700

    Sorting values

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,18 +2,11 @@
 
 const values = process.argv.slice(2);
 let result = 0;
-let trav;
-console.log(values);
-console.log(values.length);
+
 if (values.length === 0 || values.length === 1) {
   console.log(0);
-  throw '';
+} else {
+  values.sort(function(a, b) {return b - a});
+  result = values[1];
+  console.log(result);
 }
-
-for (trav = 0; trav < values.length; trav++) {
-  if (result <= parseInt(values[trav])) {
-    result = parseInt(values[trav]);
-  }
-}
-
-console.log(result);
