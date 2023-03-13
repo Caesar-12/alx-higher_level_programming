88cb038eb768e05a28348533c1bdc449eff2dc42
Caesar-12 <idowuenoch65@gmail.com>
2023-03-13 15:21:04 -0700

    Sorting values

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ let result = 0;
 if (values.length === 0 || values.length === 1) {
   console.log(0);
 } else {
-  values.sort(function(a, b) {return b - a});
+  values.sort(function (a, b) { return b - a; });
   result = values[1];
   console.log(result);
 }
