b51b422228af1608bffe5afd7a6d1db19bc93cbe
Caesar-12 <idowuenoch65@gmail.com>
2023-03-14 07:25:12 -0700

    Methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+  /* get width () {
+    return this._width;
+  }
+  set width (value) {
+    if (value <= 0) {
+      return undefined;
+    }
+    this._width = value;
+  }
+  get height () {
+    return this._height;
+  }
+  set height (value) {
+    if (value <= 0) {
+      return undefined;
+    }
+    this._height = value;
+  } */
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
