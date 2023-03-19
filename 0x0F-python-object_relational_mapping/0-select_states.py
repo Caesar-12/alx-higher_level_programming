6ec5134147cb1bb33438a033db3dd4d72d78f7ba
Caesar-12 <idowuenoch65@gmail.com>
2023-03-19 10:02:26 -0700

    MySWLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Contains module that lists states in hbtn_0e_0_usa database
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect('localhost:3306', argv[1], argv[2], argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id')
