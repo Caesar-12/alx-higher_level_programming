b4f8b3f094525e6a6f5b7f045afaabc3a2b19bcc
Caesar-12 <idowuenoch65@gmail.com>
2023-03-30 19:26:37 -0700

    Displays status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays status code
-curl -I -s "$1" | grep "HTTP" | cut -d " " -f 2
+# Displays status code #curl -I -s "$1" | grep "HTTP" | cut -d " " -f 2
+curl -s -o /dev/null -w "%{http_code}\n" "$1"
